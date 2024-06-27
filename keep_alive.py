964dd2448527eab66a5896ed6b5e1a7865328c02
punishersam9 <guptaavi690@gmail.com>
2024-06-27 21:52:21 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,44 +1,14 @@
-from flask import Flask
-import random
-import threading
+from flask import Flask, render_template
+from threading import Thread
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Alive"
 
-number = 0
+def run():
+    app.run(host='0.0.0.0',port=8080)
 
-@app.route("/")
-def hello_world():
-    """ URL returns random number """
-    return str(number)
-
-
-def gen_rand():
-    """ generating random numbers """
-    global number
-    while True:
-        number = random.randint(0, 5)
-        time.sleep(2)
-
-if __name__ == '__main__':
-    # starting thread to generate the number
-    x = threading.Thread(target=gen_rand, daemon=True)
-    x.start()
-
-    # starting web server
-    app.run()
-
-
-# from flask import Flask, render_template
-# from threading import Thread
-
-# app = Flask(__name__)
-# @app.route('/')
-# def index():
-#     return "Alive"
-
-# def run():
-#     app.run(host='0.0.0.0',port=8080)
-
-# def keep_alive():
-#     t = Thread(target=run)
-#     t.start()    
+def keep_alive():
+    t = Thread(target=run)
+    t.start()    
